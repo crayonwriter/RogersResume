c1191d31c776139ef03b07ffdff29695135f73aa
Theda Rogers <crayonwriter@gmail.com>
2019-03-18 17:47:42 -0400

    feat: add fragment for tech skills

---
+++ app/src/main/java/com/thedakrogers/rogersresume/TechSkills.java
@@ -0,0 +1,29 @@
+package com.thedakrogers.rogersresume;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TechSkills extends Fragment {
+
+
+    public TechSkills() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tech_skills, container, false);
+    }
+
+}
